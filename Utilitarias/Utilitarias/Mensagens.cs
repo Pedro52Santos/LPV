4e70faddcd6ea54b9d15e4181b2ee0a125142a9d
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-05-06 15:11:24 -0400

    Pasta de Bibliotecas

---
+++ Utilitarias/Utilitarias/Mensagens.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Utilitarias
+{
+    public class Mensagens
+    {
+        private string titulo;
+        public Mensagens(string titulo)
+        {
+            this.titulo = titulo;
+        }
+
+        public void MsgAlerta(string mensagem)
+        {
+            MessageBox.Show(mensagem, titulo, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        public void MsgInfirmacao(string mensagem)
+        {
+            MessageBox.Show(mensagem, titulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void MsgErro(string mensagem)
+        {
+            MessageBox.Show(mensagem, titulo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public string MsgPergunta(string pergunta)
+        {
+            DialogResult resposta = MessageBox.Show(pergunta, titulo, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return resposta.ToString();
+        }
+    }
+}
