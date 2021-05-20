a3dbad53149da7292e31df6efd31d874550c46ac
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-05-20 11:34:57 -0400

    fim da aula 26
    iniciar a 27 em casa por erro de bd no trabalho.

--- ProjetoCarros/ProjetoCarros/Form1.Designer.cs
+++ ProjetoCarros/ProjetoCarros/Form1.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(68)))), ((int)(((byte)(228)))));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label1.Location = new System.Drawing.Point(51, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(396, 70);
@@ -70,6 +70,7 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button4.Location = new System.Drawing.Point(507, 244);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(132, 60);
@@ -88,6 +89,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button3.Location = new System.Drawing.Point(507, 135);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(132, 60);
@@ -106,6 +108,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button2.Location = new System.Drawing.Point(507, 354);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 60);
@@ -124,6 +127,7 @@
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button1.Location = new System.Drawing.Point(507, 30);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 60);
--- ProjetoCarros/ProjetoCarros/Formularios/FormConsultar.Designer.cs
+++ ProjetoCarros/ProjetoCarros/Formularios/FormConsultar.Designer.cs
@@ -28,20 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.edPlaca = new System.Windows.Forms.TextBox();
+            this.edModelo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.edIdade = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edValor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edSituacao = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.cbPesquisa = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btPesquisa = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // edPlaca
+            // 
+            this.edPlaca.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.edPlaca.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edPlaca.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.edPlaca.Location = new System.Drawing.Point(412, 167);
+            this.edPlaca.Name = "edPlaca";
+            this.edPlaca.Size = new System.Drawing.Size(324, 27);
+            this.edPlaca.TabIndex = 23;
+            // 
+            // edModelo
+            // 
+            this.edModelo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.edModelo.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edModelo.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.edModelo.Location = new System.Drawing.Point(55, 167);
+            this.edModelo.Name = "edModelo";
+            this.edModelo.Size = new System.Drawing.Size(324, 27);
+            this.edModelo.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(409, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 18);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Placa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(52, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Modelo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.label1.Location = new System.Drawing.Point(49, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 35);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Consutar veículo\r\n";
+            // 
+            // edIdade
+            // 
+            this.edIdade.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.edIdade.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edIdade.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.edIdade.Location = new System.Drawing.Point(55, 224);
+            this.edIdade.Name = "edIdade";
+            this.edIdade.Size = new System.Drawing.Size(324, 27);
+            this.edIdade.TabIndex = 25;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(52, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Idade:";
+            // 
+            // edValor
+            // 
+            this.edValor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.edValor.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edValor.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.edValor.Location = new System.Drawing.Point(412, 224);
+            this.edValor.Name = "edValor";
+            this.edValor.Size = new System.Drawing.Size(324, 27);
+            this.edValor.TabIndex = 27;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(409, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 18);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Valor Atual:";
+            // 
+            // edSituacao
+            // 
+            this.edSituacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.edSituacao.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edSituacao.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.edSituacao.Location = new System.Drawing.Point(55, 275);
+            this.edSituacao.Name = "edSituacao";
+            this.edSituacao.Size = new System.Drawing.Size(324, 27);
+            this.edSituacao.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(52, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 18);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Situação:";
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(68)))), ((int)(((byte)(228)))));
+            this.btLimpar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btLimpar.FlatAppearance.BorderSize = 0;
+            this.btLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLimpar.ForeColor = System.Drawing.Color.White;
+            this.btLimpar.Location = new System.Drawing.Point(412, 275);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(324, 27);
+            this.btLimpar.TabIndex = 30;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btLimpar.UseVisualStyleBackColor = false;
+            // 
+            // cbPesquisa
+            // 
+            this.cbPesquisa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.cbPesquisa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPesquisa.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.cbPesquisa.FormattingEnabled = true;
+            this.cbPesquisa.Location = new System.Drawing.Point(55, 106);
+            this.cbPesquisa.Name = "cbPesquisa";
+            this.cbPesquisa.Size = new System.Drawing.Size(586, 28);
+            this.cbPesquisa.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(52, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 18);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Consultar por placa:";
+            // 
+            // btPesquisa
+            // 
+            this.btPesquisa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(68)))), ((int)(((byte)(228)))));
+            this.btPesquisa.BackgroundImage = global::ProjetoCarros.Properties.Resources.Search;
+            this.btPesquisa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btPesquisa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btPesquisa.FlatAppearance.BorderSize = 0;
+            this.btPesquisa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPesquisa.ForeColor = System.Drawing.Color.White;
+            this.btPesquisa.Location = new System.Drawing.Point(642, 104);
+            this.btPesquisa.Name = "btPesquisa";
+            this.btPesquisa.Size = new System.Drawing.Size(94, 31);
+            this.btPesquisa.TabIndex = 33;
+            this.btPesquisa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btPesquisa.UseVisualStyleBackColor = false;
+            this.btPesquisa.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormConsultar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(794, 345);
+            this.Controls.Add(this.btPesquisa);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbPesquisa);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.edSituacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.edValor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edIdade);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.edPlaca);
+            this.Controls.Add(this.edModelo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FormConsultar";
             this.Text = "FormConsultar";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox edPlaca;
+        private System.Windows.Forms.TextBox edModelo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edIdade;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox edValor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edSituacao;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.ComboBox cbPesquisa;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btPesquisa;
     }
 }
